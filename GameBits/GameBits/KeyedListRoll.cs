ccc66ec1c3bb6cc35ddc48cc5c76082b20e3b4ed
DougLeary <DougLeary@gmail.com>
2017-02-13 02:09:52 -0800

    cleanup and notes

--- GameBits/GameBits/KeyedListRoll.cs
+++ GameBits/GameBits/KeyedListRoll.cs
@@ -51,22 +51,3 @@ namespace GameBits
     }
 }
 
-/*
- * TODO: need a syntax for specifying a KeyedListRoll in a RollableTable;
- * Example:
- *		<KeyedListRoll TableName="Monster Treasure" Key="D" Rolls="2" />.
- * Also need a string designator for a Treasure Type in a Monster description;
- * Example:
- *		"A,C,2H" would generate 3 KeyedListRolls: (Key="A", Rolls=1), (Key="C", Rolls=1), (Key="H", Roll=2);
- * 
- * Come to think of it, RollableTable and KeyedList should both be descendants of an abstract type ResolvableTable.
- * ResolvableTable would have a Dictionary<T, string> and an abstract method GetItem(<T> keyValue).
- * KeyedList would use Dictionary<string, string>
- * RollableTable would use Dictionary<int, string>
- * KeyedList's GetItem method would use the base Dictionary's string match.
- * RollableTable's GetItem would sequentially find the first item whose key is >= keyValue.
- * 
- * The original reason for implementing RollableTable as a DataTable containing RollableTableRows was
- * to make it easily bindable to ASP.Net web controls; with AJAX/JSON/SOA this is not a design issue anymore. 
-
-*/
